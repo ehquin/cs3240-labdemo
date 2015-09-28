4921d8b8625e9ffd422a0a34b428c3f216af6a77
Liz Quin <ehq3bh@virginia.edu>
2015-09-28 17:17:17 -0400

    we are in master edit hello and newpython

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from helper import greeting
+from newpython import grillz
 
 if __name__ == '__main__':
     greeting("hello")
+    print(grillz())
---
+++ newpython.py
@@ -0,0 +1,3 @@
+
+def grillz():
+    return "grillz"
